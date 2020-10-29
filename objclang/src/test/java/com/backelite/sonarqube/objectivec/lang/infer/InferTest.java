d78483dc0ca64bf82a55f423513d54af3f79f1b7
Harry <wuwenguang@tal.com>
2020-10-29 18:50:08 +0800

    add infer rule

---
+++ objclang/src/test/java/com/backelite/sonarqube/objectivec/lang/infer/InferTest.java
@@ -0,0 +1,27 @@
+package com.backelite.sonarqube.objectivec.lang.infer;
+
+import com.backelite.sonarqube.objectivec.issues.infer.InferRulesDefinition;
+import org.junit.Test;
+import org.sonar.api.SonarQubeSide;
+import org.sonar.api.SonarRuntime;
+import org.sonar.api.internal.SonarRuntimeImpl;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.utils.Version;
+
+/**
+ * @author Harry
+ * @date 2020/10/28
+ */
+public class InferTest {
+
+    @Test
+    public void define() {
+        SonarRuntime sonarRuntime = SonarRuntimeImpl.forSonarQube(Version.create(7, 3), SonarQubeSide.SERVER);
+        InferRulesDefinition rulesDefinition = new InferRulesDefinition();
+        RulesDefinition.Context context = new RulesDefinition.Context();
+        rulesDefinition.define(context);
+
+        RulesDefinition.Repository inferRepository = context.repository("Infer");
+        System.out.println(inferRepository.toString());
+    }
+}
